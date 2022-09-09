cfe33500f406c509d46a5ad8d9f2bd8575b24ec7
Vydah <soty.harry@gmail.com>
2022-09-09 04:42:31 -0700

    update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * Description: main - Prints out  string to stdout.
- * Return: 0 if success.
- * /
+ *  Return: 0 if success.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
