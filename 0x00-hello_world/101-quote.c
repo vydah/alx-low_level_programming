9ed08ffd68be7eef2218590c624828248df52d79
Vydah <soty.harry@gmail.com>
2022-09-10 03:06:03 -0700

    create 101 and 100

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main: prints out a quote
+ * Return: 1 if successfull
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
+
+
