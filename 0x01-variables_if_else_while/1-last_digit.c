ea10fb768ac7ff93ff3375f1cb299d33d4ac8425
vydah <soty.harry@gmail.com>
2022-09-12 10:40:27 -0700

    message

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+ /* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/** main- prints the last digit of the number n
+ * return 0- if success
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
