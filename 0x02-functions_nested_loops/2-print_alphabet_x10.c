255f9ab757bfc2594b092a8fa73ff29bdd73dd98
vydah <55145911+vydah@users.noreply.github.com>
2022-09-13 21:12:00 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- *  print_alphabet- prints all the alphabets in lower cases 10 times
+ *  void print_alphabet_x10- prints all the alphabets in lower cases 10 times
  *  Return: nothing
  */
 
 void print_alphabet_x10(void)
 {
 	int n;
+
 	for (n = 0; n < 9; n++)
 	{
 		char c = 'a';
+
 		while (c < 'z')
 		{
 			_putchar (c);
 			c++;
 		}
+		_putchar ('\n');
 	}
-	_putchar ('\n');
 }
