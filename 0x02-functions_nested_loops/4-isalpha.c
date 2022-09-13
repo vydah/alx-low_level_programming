0e233a70af4c368ab789f5aa6c35bdc3edfa2909
vydah <55145911+vydah@users.noreply.github.com>
2022-09-13 22:59:11 +0100

    update 4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  _isalpha- checks if c is lowercase
+ * @c: character to test
+ *  Return: 1 if c is lower case, 0 if c otherwise
+ */
+
+int _isalpha(int c);
+{
+	int r;
+
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' )
+	{
+		r = 1;
+	}
+	else
+	{
+		r = 0;
+	}
+	return (r);
+}
