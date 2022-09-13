95c14aae8d933edefcbee903b0967c70f82bb4ff
vydah <55145911+vydah@users.noreply.github.com>
2022-09-13 23:47:50 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,21 +8,22 @@
 
 int print_sign(int n)
 {
-    int r;
-    if (n > 0)
-    {
-        r = 1;
-        _putchar('+');
-    }
-    else if (n == 0)
-    {
-        r = 0;
-        _putchar('0');
-    }
-    else
-    {
-        r = -1;
-        _putchar('-');
-    }
-    return (r);
+        int r;
+
+        if (n > 0)
+        {
+            r = 1;
+            _putchar('+');
+        }
+        else if (n == 0)
+        {
+            r = 0;
+            _putchar('0');
+        }
+        else
+        {
+            r = -1;
+            _putchar('-');
+        }
+        return (r);
 }
