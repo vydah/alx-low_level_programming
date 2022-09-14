a32d816d70e559b7d7fc486c814627a52d622176
vydah <55145911+vydah@users.noreply.github.com>
2022-09-14 02:10:59 +0100

    Update 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - Gives the last digit of a of i.
+ * @i: the number to be checked.
+ * Return: n
+ */
+int print_last_digit(int i)
+{
+	int n = i % 10;
+
+	return (n);
+}
