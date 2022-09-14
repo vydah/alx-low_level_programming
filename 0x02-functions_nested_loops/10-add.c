5fbb7ec40ccad497d951a57115fece6ed415428e
vydah <55145911+vydah@users.noreply.github.com>
2022-09-14 20:53:42 +0100

    Update 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * times_table - Prints the 9 timestable, starting with zero.
+ * @i: one number to be added
+ * @j: second number to be added
+ * Return: 0
+ */
+int add(int i, int j)
+{
+	int add;
+
+	add = (i + j) * 10;
+	_putchar((add % 10) + '0');
+	return (0);
+}
