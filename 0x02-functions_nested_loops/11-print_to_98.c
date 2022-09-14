bb275bf8d323cf9278cb1e7e4d1e2a3c5e4580ce
vydah <55145911+vydah@users.noreply.github.com>
2022-09-15 00:37:30 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,37 +1,44 @@
 #include "main.h"
 /**
- * print_to_98 - Prints all natural numbers from n to 98, followed by a new line.
+ * print_to_98 - Prints all natural numbers from n to 98.
  * @n: call value
  */
 void print_to_98(int n)
 {
     int i = n;
 
-	while(i <= 98)
-    {
-        if (i == n && n < 10)
+	while (i <= 98 || i >= 98)
         {
-            _putchar(i + '0');
+            if (i == n && n < 10)
+            {
+                _putchar(i + '0');
+            }
+            else if (i == n && n >= 10)
+            {
+                _putchar((i / 10) + '0');
+                _putchar((i % 10) + '0');
+            }
+            else if(i > n && n < 10 )
+            {
+                _putchar(',');
+                _putchar(' ');
+                _putchar(i + '0');
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+                _putchar(((i * 1) / 10) + '0');
+                _putchar((i % 10) + '0');
+            }
+            if(n < 98)
+            {
+                i++;
+            }
+            else
+            {
+                i--;
+            }
         }
-        else if (i == n && n >= 10)
-        {
-            _putchar((i / 10) + '0');
-            _putchar((i % 10) + '0');
-        }
-        else if(i > n && n < 10 )
-        {
-            _putchar(',');
-            _putchar(' ');
-            _putchar(i + '0');
-        }
-        else
-        {
-            _putchar(',');
-            _putchar(' ');
-            _putchar(((i * 1) / 10) + '0');
-            _putchar((i % 10) + '0');
-        }
-       i++;
-    }
-    _putchar('\n');
+     _putchar('\n');
 }
