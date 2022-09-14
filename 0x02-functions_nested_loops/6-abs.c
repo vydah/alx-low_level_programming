3140b82ff19bb23603246f7c915061ab2f844f57
vydah <55145911+vydah@users.noreply.github.com>
2022-09-14 02:01:52 +0100

    Update 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int _abs(int) - Determines whether an integer is positive, negative or zero.
+ * @i: the number to be checked.
+ * Return: 1 if positive, 0 if zero, -1 if negative, / if not a digit.
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = i * -1;
+	}
+	return ('i');
+}
