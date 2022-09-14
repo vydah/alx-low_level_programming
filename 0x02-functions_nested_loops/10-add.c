cd2eede6a8a3f51c5445af75202b602872824d86
vydah <55145911+vydah@users.noreply.github.com>
2022-09-14 22:58:21 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,22 +3,9 @@
  * add - Prints the sum of two numbers.
  * @i: one number to be added
  * @j: second number to be added
- * Return: 0
+ * Return: i+j
  */
 int add(int i, int j)
 {
-	int add;
-
-	add = i + j;
-
-	if (add < 10)
-	{
-		_putchar(add * 1);
-	}
-	else
-	{
-		_putchar((add / 10) + '0');
-		_putchar((add % 10) + '0');		
-	}
-	return (0);
+	return (i + j);
 }
