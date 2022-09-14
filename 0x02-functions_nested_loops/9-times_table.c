3c1326cbdce709d463d356b548fa01c4d0489119
vydah <55145911+vydah@users.noreply.github.com>
2022-09-14 22:30:04 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,33 +11,28 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (j == 9)
+			if (j ==0)
 			{
-				if ((i * j) < 10)
-				{
-					_putchar((i * j) + '0');
-				}
-				else
-				{
-					_putchar(((i * j) / 10) + '0');
-					_putchar(((i * j) % 10) + '0');
-				}
+				_putchar((i * j) + '0');
+				_putchar(',');
 			}
 			else
 			{
 				if ((i * j) < 10)
 				{
+					_putchar(' ');
+					_putchar(' ');
 					_putchar((i * j) + '0');
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+					
 				}
 				else
 				{
+					_putchar(' ');
 					_putchar(((i * j) / 10) + '0');
 					_putchar(((i * j) % 10) + '0');
 					_putchar(',');
-					_putchar(' ');
+					
 				}
 				
 			}
