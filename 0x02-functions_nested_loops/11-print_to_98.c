a35469081fe1bd81ee85b48c2f28f8bf698fd38a
vydah <55145911+vydah@users.noreply.github.com>
2022-09-14 23:14:47 +0100

    Update 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_to_98 - Prints all natural numbers from n to 98, followed by a new line.
+ * @n: call value
+ */
+void print_to_98(int n)
+{
+	for(i = n; i <=98; i++ )
+    {
+        if (i == n && n < 10)
+        {
+            _putchar(i + '0');
+        }
+        else if (i == n && n >= 10)
+        {
+            _putchar((i / 10) + '0');
+            _putchar((i % 10) + '0');
+        }
+        else if(i > n && n < 10 )
+        {
+            _putchar(',');
+            _putchar('');
+            _putchar(i + '0');
+        }
+        else
+        {
+            _putchar(',');
+            _putchar('');
+            _putchar((i / 10) + '0');
+            _putchar((i % 10) + '0');
+        }
+        
+    }
+}
