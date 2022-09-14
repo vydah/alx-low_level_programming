0b749cd1769f86b566a1bc80dc9a8084b89cac93
vydah <55145911+vydah@users.noreply.github.com>
2022-09-14 23:16:44 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,6 +5,8 @@
  */
 void print_to_98(int n)
 {
+    int i;
+    
 	for(i = n; i <=98; i++ )
     {
         if (i == n && n < 10)
@@ -19,13 +21,13 @@ void print_to_98(int n)
         else if(i > n && n < 10 )
         {
             _putchar(',');
-            _putchar('');
+            _putchar(' ');
             _putchar(i + '0');
         }
         else
         {
             _putchar(',');
-            _putchar('');
+            _putchar(' ');
             _putchar((i / 10) + '0');
             _putchar((i % 10) + '0');
         }
