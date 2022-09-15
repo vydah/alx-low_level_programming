55d47584d0c39cb2cdf73e62285f8a7d99098e72
vydah <55145911+vydah@users.noreply.github.com>
2022-09-15 01:50:24 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,13 +16,15 @@ void print_to_98(int n)
         for (i = n; i < 98; i++)
         {
             printf("%d, ", i);
+            printf("98");
         }
     }
     else if ( n > 98)
     {
-        for (i = n; i >= 98; i--)
+        for (i = n; i > 98; i--)
         {
             printf("%d, ", i);
+            printf("98");
         }
     }
     else
