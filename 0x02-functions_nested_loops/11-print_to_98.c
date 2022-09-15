643d4e2798191d4307e66e86e3da7f1a0ec24519
vydah <55145911+vydah@users.noreply.github.com>
2022-09-15 01:31:48 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,10 +3,26 @@
 
 /**
  *  print_to_98- prints the string _putchar
+ * @n: input value
  *  Return: 0 if success
  */
 
 void print_to_98(int n)
 {
-    printf("\n");
+    int i;
+
+    if(n <= 98)
+    {
+        for (i = n; i <= 98; i++)
+        {
+            printf("%d, ", i);
+        }
+    }
+    else
+    {
+        for (i = n; i >= 98; i--)
+        {
+            printf("%d, ", i);
+        }
+    }
 }
