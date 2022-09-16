0d454329c72ebf83341b57d55f8e1257b4d08e5b
vydah <55145911+vydah@users.noreply.github.com>
2022-09-17 00:04:08 +0100

    a lot

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *@c: call variable.
+ * Return: 1 if uppercase 0 if otherwise.
+ */
+int _isupper(int c)
+{
+    int i;
+    int r;
+
+    for(i='65'; i <= '90', i++)
+    {
+        if(c == i)
+            r = 1;
+        else
+            r = 0;
+    }
+    return (r);
+}
