3746c32ec6363297ad4065dc173296240a6c3477
vydah <55145911+vydah@users.noreply.github.com>
2022-09-21 09:47:52 +0100

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints every other string followed by a new line.
+ * @str: call value
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+		str++;
+	}
+	_putchar ('\n');
+}
