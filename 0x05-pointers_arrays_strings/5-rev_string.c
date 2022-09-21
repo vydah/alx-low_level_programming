80e08e885eeade17edd74de65d9ab46c1d9bca4a
vydah <55145911+vydah@users.noreply.github.com>
2022-09-21 09:32:12 +0100

    update

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rev_string - prints a string followed by a new line.
+ * @str: call value
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	char r[1000];
+
+
+	i = 0;
+	j = 0;
+	while (s[i] != '\0')
+	{
+		r[i] = s[i];
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		s[i] = r[j];
+		i--;
+		j++;
+	}
+	s[j++] = '\0';
+}
