f98754427165d0969f6a754c1846cc6279638fb5
vydah <55145911+vydah@users.noreply.github.com>
2022-09-23 20:45:16 +0100

    update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,29 @@
 #include "main.h"
 
 /**
+ * *_strcat- prints the concatenation of two string
+ * @dest: one of the string
+ * @another string
+ * Return: a pointer to the concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char concat;
+	int i = 0;
+	char *d = &dest;
+
+	while (*dest[i] != '\0')
+	{
+		concat[i] = dest[i];
+		i++;
+	}
+	while (*src[i] != '\0')
+	{
+		concat = + *src[i];
+		i++
+	}
+	*dest = concat;
+	Return (*d);
+}
+
