a45ff4ad4732839f21063f3577cf5361533220f5
vydah <55145911+vydah@users.noreply.github.com>
2022-09-23 23:57:38 +0100

    Update 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+/**
+ * _strncpy - copies the string in src into dest
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ * @n: the length of the string src
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i <= n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
