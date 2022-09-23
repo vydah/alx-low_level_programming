483f90cb8062e8e1200c520b9af6594c710270be
vydah <55145911+vydah@users.noreply.github.com>
2022-09-23 13:19:48 +0100

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,14 +10,13 @@ void print_number(int n)
 
 	res = n;
 	expo = len =  1;
-/*Check negatives*/
+
 	if (res < 0)
 	{
 		res *= -1;
 		_putchar('-');
 	}
 
-/**/
 	temp = res;
 	while (temp >= 10)
 	{
@@ -25,10 +24,8 @@ void print_number(int n)
 		temp /= 10;
 	}
 
-/*Create Exponent*/
 	for (i = 1; i < len; i++)
 		expo *= 10;
-/*Main */
 	while (expo > 1)
 	{
 		_putchar((res / expo) % 10 + '0');
