2fb52823f695834c693a5c2e66adbc94a0ac0020
vydah <55145911+vydah@users.noreply.github.com>
2022-09-24 01:04:20 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,7 +10,6 @@ void print_number(int n)
 	int res, temp, expo;
 
 	expo = 1;
-/*Check negatives*/
 	if (n >= 0)
 		res = n * -1;
 	else
@@ -18,15 +17,12 @@ void print_number(int n)
 		res = n;
 		_putchar('-');
 	}
-
-/*Initialize exponent variable*/
 	temp = res;
 	while (temp <= -10)
 	{
 		expo *= 10;
 		temp /= 10;
 	}
-/*Main */
 	while (expo >= 1)
 	{
 		_putchar(((res / expo) % 10) * -1 + '0');
