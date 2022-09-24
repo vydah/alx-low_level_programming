f54c602bd9c5229f448bec23a428da34a760fda0
vydah <55145911+vydah@users.noreply.github.com>
2022-09-24 01:40:11 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,12 +14,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int i, j, n;
 	
 	i = j = n = 0;
-	if ((n1[0] - '0') + (n2[0] - '0') <= 10)
+	if ((n1[0] - '0') + (n2[0] - '0') >= 10)
 	{
 		r[0] = 1 + '0';
 		j = 1;
 	}
-	while (i < size_r && (n1[i] != '\0' || n2[i] != '\0' || r[j] != '\0'))
+	while (i < size_r && (n1[i] != '\0' || n2[i] != '\0'))
 	{
 		if ((n1[i + 1] - '0') + (n2[i + 1] - '0') >= 10)
 			n = 1;
