389c0ba0838da98b5e8a6a3c6a39c300d2051a6a
vydah <55145911+vydah@users.noreply.github.com>
2022-09-24 01:15:19 +0100

    update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *string_toupper(char *c);
 int _putchar(int c);
+void print_number(int n);
 
 #endif /* MAIN_H */
