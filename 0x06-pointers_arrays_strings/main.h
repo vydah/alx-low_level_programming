2d6b3cd5dd2ab986e62476eba09f43af85637d11
vydah <55145911+vydah@users.noreply.github.com>
2022-09-24 01:19:04 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_strncat(char *dest, char *src, int n);
 char *string_toupper(char *c);
 int _putchar(int c);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /* MAIN_H */
