83b63d3c3d704996912bff2cfa6d759e24d528a1
vydah <55145911+vydah@users.noreply.github.com>
2022-09-28 12:26:33 +0100

    update recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer to the source string
+ * Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This is none empty read me
