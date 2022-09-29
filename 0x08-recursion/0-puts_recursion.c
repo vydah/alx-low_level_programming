68286f8e351bd273bcb25952d48466f99f8b7ac7
vydah <55145911+vydah@users.noreply.github.com>
2022-09-29 19:00:57 +0100

    update recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - prints a string with recursion
+ * @s: string to print
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+}
+	}
+	
