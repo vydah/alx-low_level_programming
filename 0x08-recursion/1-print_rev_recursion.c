d16b7e645e399e978ab7b61680d6cf6eb550edee
vydah <55145911+vydah@users.noreply.github.com>
2022-09-29 19:20:36 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,7 +3,7 @@
  * _print_rev_recursion - prints a string in reverse with recursion
  * @s: string to print
  */
- void _print_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
