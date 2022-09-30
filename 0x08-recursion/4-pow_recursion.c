5f47c1efc39fa6e1a7a382b00c24b5ac380f4ebd
vydah <55145911+vydah@users.noreply.github.com>
2022-09-30 11:46:14 +0100

    Create 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _pow_recursion - prints x to the power of y
+ * @x: the number
+ * @y: the power
+ * Return: value of x raised to power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (x * factorial(x, y-1));
+	}
+}
