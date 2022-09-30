c0960134d60aaa709639267578bbb69698d7b6a9
vydah <55145911+vydah@users.noreply.github.com>
2022-09-30 10:49:21 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,14 @@ int _strlen_recursion(char *s)
 {
 	if (s[0] == '\0')
 	{
-		return(0);
+		return (0);
+	}
+	else if (s[1] == '\0')
+	{
+		return (1);
 	}
 	else
 	{
-		return(1 + _strlen_recursion(s));
+		return (1 + _strlen_recursion(s));
 	}
 }
