275ee42f54e0e34ef17ca0958391d350873925a4
vydah <55145911+vydah@users.noreply.github.com>
2022-09-30 10:10:57 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,21 +2,17 @@
 /**
  * _strlen_recursion - prints a string with recursion
  * @s: string to count
- * Return: count which is the lenght of the string
+ * Return: the lenght of the string
  */
 int _strlen_recursion(char *s)
 {
-	int count = 0;
-
 	if (*s == '\0')
 	{
-		count = count + 0;
+		return (0);
 	}
 	else
 	{
 		count = count + 1;
-		s++;
-		_strlen_recursion(s);
+		return(1 + _strlen_recursion(s));
 	}
-	return (count);
 }
