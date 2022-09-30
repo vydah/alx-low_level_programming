50b988124ac1f08d3d2494b52cfdb3c855beae7a
vydah <55145911+vydah@users.noreply.github.com>
2022-09-30 11:00:03 +0100

    Create 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - prints the factorial of a number n
+ * @n: the number
+ * Return: the factorial
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return;
+	}
+
+	else
+	{
+		return (n x factorial(n - 1));
+	}
+}
