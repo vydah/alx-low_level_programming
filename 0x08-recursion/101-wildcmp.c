fb6c3d56ac9977afe2287108d3ef00643dc8fabb
vydah <55145911+vydah@users.noreply.github.com>
2022-10-04 19:27:17 +0100

    Create 101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * wildcmp - checks if two strings are alike.
+ * @s1: string to check
+ * @s2: string to check
+ * Return: 1 identical, 0 not identical
+ */
+int wildcmp(char *s1, char *s2)
+{
+    if ((s2[0] == * || s2[1] == 0) && s1[0] != s2[0])
+    {
+        int wildcmp(s1,s2++)
+    }
+    else if (s1[0] == s2[0])
+    {
+        return (1);
+    }
+    else
+    {
+        return 0;
+    }
+}
